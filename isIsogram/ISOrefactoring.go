ca137f76d8dfcedcde22346cfd9f6ca25450142d
AmirMahDi Behjatifar <88620796+AmirMahDiBehjatifar@users.noreply.github.com>
2025-08-15 10:14:05 +0330

    Create ISOrefactoring.go

---
+++ isIsogram/ISOrefactoring.go
@@ -0,0 +1,19 @@
+package isogram
+import (
+    "strings"
+)
+func IsIsogram(word string) bool {
+	word = strings.ToUpper(word)
+
+    for i,c := range word {
+        if c == " " || c == "-" {
+            continue
+        }
+        for j := i+1;j<len(word) ; j++ {
+            if word[i] == word[j]{
+                return false
+            }
+        }
+    }
+    return true
+}
